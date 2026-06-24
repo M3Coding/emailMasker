88a80a7d21dc84344c6a450b9f7931cb060821e6
M3Coding <meady2009@gmail.com>
2026-06-23 21:18:16 -0400

    completed fortune teller task

---
+++ fortuneTeller.js
@@ -0,0 +1,28 @@
+const fortune1= "Your cat will look very cuddly today.";
+const fortune2= "The weather will be nice tomorrow.";
+const fortune3= "Be cautious of your new neighbors.";
+const fortune4= "You will find a new hobby soon.";
+const fortune5= "It would be wise to avoid the color red today.";
+let max = 5;
+let min =1;
+let randomNumber = Math.floor(Math.random() * (max-min+1)+1);
+let selectedFortune = "" ;
+console.log(selectedFortune);
+console.log(randomNumber)
+
+if(randomNumber === 1) {
+    selectedFortune += fortune1;
+    console.log(selectedFortune);
+} else if(randomNumber === 2) {
+    selectedFortune += fortune2;
+    console.log(selectedFortune);
+}else if(randomNumber === 3) {
+    selectedFortune += fortune3;
+    console.log(selectedFortune);
+}else if(randomNumber === 4) {
+    selectedFortune += fortune4;
+    console.log(selectedFortune);
+}else if(randomNumber === 5) {
+    selectedFortune += fortune5;
+    console.log(selectedFortune);
+}
