e8e5836da4d09af1cae75570bb8c40032cf15fb6
M3Coding <meady2009@gmail.com>
2026-06-24 13:58:42 -0400

    2000 year error fix

--- leapYear.js
+++ leapYear.js
@@ -1,17 +1,21 @@
-let year = 2020;
+let year = 2021;
 
 function isLeapYear(num){
-    if(num%4===0 || num%400===0) {
-        if(num%100 === 0) {
-            console.log("failed")
-            return `${year} is not a leap year.`
+    if(num%4===0 && num%100 ===0) {
+        if(num%400 === 0) {
+            console.log("I may be divisble by 100 but I am also divisible by 400 which make me a leap year")
+            return `${num} is a leap year.`
         } else {
-            console.log("success")
-            return `${year} is a leap year.`
+            console.log("failed")
+        return `${num} is not a leap year.`
         }
+        
+    } else if(num%4===0 || num%400 === 0){
+        console.log("success")
+        return `${num} is a leap year.`
     } else {
-        console.log("failed")
-            return `${year} is not a leap year.`
+        console.log("else failed")
+        return `${num} is not a leap year.`
     }
 }
 let result = isLeapYear(year);
